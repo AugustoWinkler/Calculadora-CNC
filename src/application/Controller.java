e0eba28cae76f0f6f1fd6e760da977a74d0beaa1
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-05-21 17:48:58 -0300

    Update Function Add Buttons

---
+++ src/application/Controller.java
@@ -0,0 +1,59 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    @FXML
+    public void handleAddMachine() {
+        try {
+            // Load the MachineInput.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/MachineInput.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage for the popup
+            Stage stage = new Stage();
+            stage.setTitle("Adicionar Máquina");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void handleAddMaterial() {
+        try {
+            // Load the MachineInput.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/MaterialInput.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage for the popup
+            Stage stage = new Stage();
+            stage.setTitle("Adicionar Máquina");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void handleAddOperacional() {
+        try {
+            // Load the MachineInput.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/OperacionalInput.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage for the popup
+            Stage stage = new Stage();
+            stage.setTitle("Adicionar Máquina");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
